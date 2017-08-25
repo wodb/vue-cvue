a024a56440a070c72a8ae7eff58295b0cc2b6c9f
machuang <632190797@qq.com>
2017-08-25 18:01:11 +0800

    * 增加路由

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 
 import Main from './main'
 
 import 'element-ui/lib/theme-default/index.css'
 import './style.css'
-
+// 使用elementui库跟vue.component()类似
 Vue.use(ElementUI)
+// 使用router
+Vue.use(VueRouter)
 
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
 
-new Vue({
-    el: '#app',
-    render:(h) => h(Main)
+let routers = [
+    {path:'/foo',component:Foo},
+    {path:'/bar',component:Bar},
+]
+
+const router = new VueRouter({
+    routers
 })
+new Vue({
+    router
+}).$mount('#app')
--- src/main.vue
+++ src/main.vue
@@ -1,63 +1,35 @@
 <template>
     <div class="box">
-        <el-row>
-            <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-        </el-row>
-        <el-row>
-            <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
-        </el-row>
-        <el-row>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-        </el-row>
-        <el-row>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-        </el-row>
-        <el-row>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-        </el-row>
+        <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="item in items" :key="item">
+                <h3>{{ item }}</h3>
+            </el-carousel-item>
+        </el-carousel>
+        <router-view></router-view>
     </div>
 </template>
 <style>
-    .el-row {
-        margin-bottom: 20px;
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        opacity: 0.75;
+        line-height: 300px;
+        margin: 0;
+        text-align: center;
     }
-    .el-row:last-child {
-        margin-bottom: 0;
+  
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
     }
-    .el-col {
-        border-radius: 4px;
-    }
-    .bg-purple-dark {
-        background: #99a9bf;
-    }
-    .bg-purple {
-        background: #d3dce6;
-    }
-    .bg-purple-light {
-        background: #e5e9f2;
-    }
-    .grid-content {
-        border-radius: 4px;
-        min-height: 36px;
-    }
-    .row-bg {
-        padding: 10px 0;
-        background-color: #f9fafc;
+  
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
     }
 </style>
 <script>
     export default {
-
+        data:function () {
+            return {items:['汉堡','肯德基','意大利面','披萨']}
+        }
     }
 </script>
